505818738a50193d59549dbf7789486f6154985f
KimSkade <uuebm@student.kit.edu>
2023-05-24 09:16:15 +0200

    .

---
+++ java/Helper/AASHelper.java
@@ -0,0 +1,42 @@
+package Helper;
+
+import org.eclipse.basyx.aas.metamodel.map.AssetAdministrationShell;
+import org.eclipse.basyx.submodel.metamodel.map.Submodel;
+
+/**
+ * class for helpers for creating AASX Files and AAS Objects
+ *
+ */
+public class AASHelper {
+    private static final String EXCEPTION_SHORT_ID = "exception_DUMMY_ID_short";
+    public static String nameToIdShort(String nameString)
+    {
+        if (!nameString.isEmpty())
+        {
+            return nameString.replaceAll("[^a-zA-Z0-9]+","_");
+
+        }
+        else return EXCEPTION_SHORT_ID;
+
+    }
+    public static String nameToIdentifier(String nameString)
+    {
+        if (!nameString.isEmpty())
+        {
+            return nameString.replaceAll("\\s","_").replace('.','_').replaceAll("[^a-zA-Z0-9]+","_");
+
+        }
+        else return EXCEPTION_SHORT_ID;
+
+    }
+
+    public static Submodel parseJSONtoSubmodel()
+    {
+        //copy from other project
+        return null;
+    }
+    public static AssetAdministrationShell parseJSONtoAAS()
+    {
+        return null;
+    }
+}
---
+++ java/New_ProcessModel/QualityData.java
@@ -0,0 +1,45 @@
+package New_ProcessModel;
+
+import org.eclipse.basyx.aas.metamodel.api.parts.asset.AssetKind;
+import org.eclipse.basyx.aas.metamodel.map.AssetAdministrationShell;
+import org.eclipse.basyx.aas.metamodel.map.descriptor.CustomId;
+import org.eclipse.basyx.aas.metamodel.map.parts.Asset;
+import org.eclipse.basyx.submodel.metamodel.api.identifier.IdentifierType;
+import org.eclipse.basyx.submodel.metamodel.map.Submodel;
+import org.eclipse.basyx.submodel.metamodel.map.identifier.Identifier;
+import org.eclipse.basyx.submodel.metamodel.map.submodelelement.SubmodelElementCollection;
+import org.eclipse.basyx.submodel.metamodel.map.submodelelement.dataelement.property.Property;
+
+public class QualityData {
+    public static void main(String[] args) {
+
+        // Assets generieren und AAS instanziieren
+        Asset QualityDataAsset = new Asset("QualityData", new CustomId("Quality Asset"), AssetKind.INSTANCE);
+        AssetAdministrationShell QualityDataShell = new AssetAdministrationShell("QualityDataShell",
+                new Identifier(), QualityDataAsset);
+
+        Submodel QualityData = new Submodel();
+
+        Procedure Prozedur1 = new Procedure("Id Prozedur 1", "Das ist die Prozedur 1", )
+        List<Procedure> Procedures = new List();
+
+        addProceduretoSubmodel(QualityData, Procedures); //Funktion?
+
+
+    }
+
+    public static void addProcedurestoAAS(Submodel QualityData, List<Procedure> Procedures) {
+
+        for(i = 0, i <= Procedures.size(), i++){
+
+            SubmodelElementCollection Procedures.get(i) = new SubmodelElementCollection(Infos aus den Proceduren);
+
+            QualityData.addSubmodelElement(Procedures.get(i));
+
+        }
+
+    }
+
+
+}
+
